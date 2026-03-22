e1ae1b6d09e594ea039c298912f4873853ee69c0
YuyaOki <ppc.yuya@gmail.com>
2026-03-22 18:44:48 +0900

    Xcode接続用

---
+++ dreamApp/dreamApp/dreamAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  dreamAppApp.swift
+//  dreamApp
+//
+//  Created by 大木祐弥 on 2026/03/22.
+//
+
+import SwiftUI
+
+@main
+struct dreamAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
